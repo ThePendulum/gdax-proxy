9e37b2661e837e1d67236a2cae624ef67c5c47ac
ThePendulum <niels.simenon@gmail.com>
2017-01-30 16:20:09 +0100

    Logging GDAX websocket connect, error and close.

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ const Gdax = require('gdax');
 const exchangeSocket = new Gdax.WebsocketClient(config.products, null, config.auth);
 
 exchangeSocket.on('open', () => {
+    note('socket', 0, 'Established connection with GDAX websocket API');
+
     exchangeSocket.socket.send(JSON.stringify({
         type: "heartbeat",
         on: config.heartbeat
     }));
+});
+
+exchangeSocket.on('close', () => {
+    note('socket', 0, 'Closed connection with GDAX websocket API');
+});
+
+exchangeSocket.on('error', error => {
+    note('socket', 2, error);
 });
 
 const server = net.createServer(client => {

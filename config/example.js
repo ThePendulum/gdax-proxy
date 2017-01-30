6b51ffcfabbf39923667424b01a6e23f79cbc449
ThePendulum <niels.simenon@gmail.com>
2017-01-30 16:10:55 +0100

    Fixed typo in example config.

--- config/example.js
+++ config/example.js
@@ -5,7 +5,7 @@ module.exports = {
     port: 1337,
     auth: {
         key: '04nNv0YMkA51LyFCkG90Q2FUD5UEmq80',
-        secret: '6oOq97IhEsIy3FyZVYVBI2AZo899djrp'
+        secret: '6oOq97IhEsIy3FyZVYVBI2AZo899djrp',
         passphrase: 'WW24RB0wSHKpF73ktv2h7NKZaqLzFOU7'
     },
     products: ['BTC-USD', 'ETH-USD'],
